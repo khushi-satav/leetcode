b955fe31410ac427e1cd03bea30b9a7c357431f5
Khushi Satav <khushisatav230790@acropolis.in>
2026-02-12 21:58:53 +0530

    Time: 2 ms (71.14%), Space: 55.7 MB (60.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,16 +3,16 @@ class Solution {
         int cand = 0;
         int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
 
-            if (count == 0) {
+            if(count == 0) {
                 cand = nums[i];
             }
 
-            if (cand == nums[i]) {
+            if(nums[i] == cand) {
                 count++;
             } else {
-                count--;     // yaha hona chahiye tha
+                count--;
             }
         }
 
