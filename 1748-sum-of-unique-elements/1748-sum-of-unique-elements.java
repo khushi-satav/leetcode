6cd191baec0e7ddda58eaf80f5784f9b401c1d0d
Khushi Satav <khushisatav230790@acropolis.in>
2026-03-23 22:29:17 +0530

    Time: 0 ms (100%), Space: 42.7 MB (97.76%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] freq = new int[101];
+
+        // count frequency
+        for(int i = 0; i < nums.length; i++){
+            freq[nums[i]]++;
+        }
+
+        int sum = 0;
+
+        // check unique elements
+        for(int i = 0; i < nums.length; i++){
+            if(freq[nums[i]] == 1){
+                sum += nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
