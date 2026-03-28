36565434551933dc13c540716e95ea10a375a6f6
Khushi Satav <khushisatav230790@acropolis.in>
2026-03-28 21:14:06 +0530

    Time: 0 ms (100%), Space: 48.2 MB (63.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-        arr[0] = arr[1] = -1;
+        int[] result = new int[2];
+        result[0] = findFirstPosition(nums, target);
+        result[1] = findLastPosition(nums, target);
+        return result;
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
+    int findFirstPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int firstPosition = -1;
 
-                if (arr[0] == -1) {
-                    arr[0] = i;  // first occurrence
-                }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-                arr[1] = i;  // keep updating last occurrence
+            if (nums[mid] == target) {
+                firstPosition = mid;
+                right = mid - 1; // left side jao
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return arr;
+        return firstPosition;
+    }
+
+    int findLastPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int lastPosition = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                lastPosition = mid;
+                left = mid + 1; // right side jao
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return lastPosition;
     }
 }
