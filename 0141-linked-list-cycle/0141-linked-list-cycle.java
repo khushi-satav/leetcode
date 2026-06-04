5c581598d51526533ea971813094e9d4e554e1a9
Khushi Satav <khushisatav230790@acropolis.in>
2026-06-05 00:05:04 +0530

    Time: 0 ms (100%), Space: 47 MB (15.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,14 +13,14 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
        ListNode slowptr=head;
        ListNode fastptr=head;
-       while(slowptr!=null && fastptr!=null && fastptr.next!=null){
+       while(fastptr!=null && fastptr.next!=null && slowptr!=null){
         slowptr=slowptr.next;
         fastptr=fastptr.next.next;
         if(slowptr==fastptr){
             return true;
         }
-       }
-       return false;
         
+               }
+               return false;
     }
 }
