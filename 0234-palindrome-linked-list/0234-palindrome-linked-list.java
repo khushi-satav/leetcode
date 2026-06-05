d480b133d82e89cda4d0f222fa2d419e82ac2e88
Khushi Satav <khushisatav230790@acropolis.in>
2026-06-05 23:55:29 +0530

    Time: 5 ms (45.46%), Space: 94.2 MB (87.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,37 +1,47 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
+    private ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
     public boolean isPalindrome(ListNode head) {
-        if(head==null) return true;
-        ListNode slow=head;
-        ListNode fast= head;
-        ListNode prev=null;
-        while(fast!=null&& fast.next!=null){
-            fast=fast.next.next;
-            ListNode nextNode=slow.next;
-            slow.next=prev;
-            prev=slow;
-            slow=nextNode;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        if(fast!=null){
-            slow=slow.next;
+
+        // Odd length list
+        if (fast != null) {
+            slow = slow.next;
         }
-        while(slow!=null){
-            if(prev.val!=slow.val){
+
+        slow = reverseList(slow);
+        fast = head;
+
+        while (slow != null) {
+
+            if (fast.val != slow.val) {
                 return false;
             }
-            prev=prev.next;
-            slow=slow.next;
+
+            fast = fast.next;
+            slow = slow.next;
         }
+
         return true;
-        
     }
 }
