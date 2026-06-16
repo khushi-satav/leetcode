d06c212a3f4870a452a72c25726aa3f9cc50b504
Khushi Satav <khushisatav230790@acropolis.in>
2026-06-16 23:51:08 +0530

    Time: 2 ms (91.97%), Space: 47.6 MB (75.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != 0) {
-                swap(nums, i, j);
-                i++;
+        int z=0;
+        for(int nz=0;nz<nums.length;nz++){
+            if(nums[nz]!=0){
+                int temp=nums[nz];
+                nums[nz]=nums[z];
+                nums[z]=temp;
+                z++;
             }
         }
-    }
-
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
