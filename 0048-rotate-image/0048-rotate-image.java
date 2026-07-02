8346a3f2f9b9429aa3467a9a518b157aaead5457
Khushi Satav <khushisatav230790@acropolis.in>
2026-07-02 14:08:59 +0530

    Time: 0 ms (100%), Space: 43.8 MB (33.15%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int j = 0;
+            int k = n - 1;
+
+            while (j < k) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                j++;
+                k--;
+            }
+        }
+    }
+}
