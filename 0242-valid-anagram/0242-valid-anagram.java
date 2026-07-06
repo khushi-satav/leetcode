8b8640afc4557013740155059cfe57718ac50eb9
Khushi Satav <khushisatav230790@acropolis.in>
2026-07-06 10:29:27 +0530

    Time: 7 ms (32.68%), Space: 44.1 MB (89.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int m=s.length();
-        int n=t.length();
-        if(m!=n){
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        s = s.replace(" ", "");
+        t = t.replace(" ", "");
+
+        if (s.length() != t.length()) {
             return false;
-        }else{
-            int count[]=new int[26];
-            for(int i=0;i<m;i++){
-                count[s.charAt(i)-'a']++;
-            }
-            for(int i=0;i<n;i++){
-                count[t.charAt(i)-'a']--;
-            }
-            for(int i=0;i<count.length;i++){
-                if(count[i]!=0){
-                    return false;
-                }
-            }
-            return true;
         }
-        
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
