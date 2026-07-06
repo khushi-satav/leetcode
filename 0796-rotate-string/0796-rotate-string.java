ed6ba54a12031bd7b84fa072dc6ab67b8290e5c9
Khushi Satav <khushisatav230790@acropolis.in>
2026-07-06 10:05:46 +0530

    Time: 1 ms (41.81%), Space: 43.5 MB (10.15%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String str = s + s;
+
+        if (str.indexOf(goal) != -1) {
+            return true;
+        }
+
+        return false;
+    }
+}
