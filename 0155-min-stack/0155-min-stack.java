e5f96b03ba63a4ac87ff41b2042a95df7f4da183
Khushi Satav <khushisatav230790@acropolis.in>
2026-07-17 10:44:35 +0530

    Time: 35 ms (32.51%), Space: 99.7 MB (68.81%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -9,29 +9,24 @@ class MinStack {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-    
-    public void push(int val) {
+    public void push(int val){
         stack.push(val);
-        // Push into minStack only if it's empty or val <= current min
-        if(minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
-        }
+        if(minStack.isEmpty()|| val<=minStack.peek())
+        minStack.push(val);
     }
-    
-    public void pop() {
-        int poppedValue = stack.pop();
-        if(poppedValue == minStack.peek()) {
-            minStack.pop();
-        }
+    public void pop(){
+        int poppedValue=stack.pop();
+        if(poppedValue==minStack.peek())
+        minStack.pop();
     }
-    
-    public int top() {
+    public int top(){
         return stack.peek();
     }
-    
-    public int getMin() {
+    public int getMin(){
         return minStack.peek();
     }
+    
+  
 }
 
 /**
