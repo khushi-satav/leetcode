db7146d3d51625708bfd5440d031e6ce7cb02c11
Khushi Satav <khushisatav230790@acropolis.in>
2026-08-25 22:34:34 +0530

    Time: 1 ms (99.94%), Space: 47 MB (46.14%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int value=0;
-        for(int i=0;i<nums.length;i++){
-            value=value^nums[i];
-        }
-        return value;
+       int res=0;
+       for(int num:nums){
+        res =res ^ num;
+       }
+       return res;
         
     }
 }
