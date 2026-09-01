9c89898f1f5f60b19786b18f5942a872aa49c53f
Khushi Satav <khushisatav230790@acropolis.in>
2026-09-01 22:40:38 +0530

    Time: 5 ms (62.67%), Space: 44.6 MB (59.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        s = s.toLowerCase();
-        t = t.toLowerCase();
-
-        s = s.replace(" ", "");
-        t = t.replace(" ", "");
-
         if (s.length() != t.length()) {
             return false;
         }
@@ -15,14 +9,11 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
             count[t.charAt(i) - 'a']--;
         }
 
-        for (int c : count) {
-            if (c != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
                 return false;
             }
         }
